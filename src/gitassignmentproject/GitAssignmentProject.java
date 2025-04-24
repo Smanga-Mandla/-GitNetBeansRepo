4d90cb1dd964f1c86c750aac378f9cf4ccee95e9
mandla Tivane <smangamandla05@gmail.com>
2025-04-24 13:00:44 +0200

    Modified Main.java in conflict-demo

--- src/gitassignmentproject/GitAssignmentProject.java
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -4,6 +4,6 @@ package gitassignmentproject;
  
 public class GitAssignmentProject {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git.");
+        System.out.println("This is the CONFLICT-DEMO branch.");
     }
 }
